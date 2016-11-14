4f0b306baffd9ee3c8667f86897226425dc30811
ced <zuger.cedric@gmail.com>
2016-11-14 13:39:59 +0100

    Right repo url.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'haute_tension'
+set :repo_url, 'git@github.com:czuger/haute_tension.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
