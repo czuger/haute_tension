e0ab1721e5d1cb8d798de57c07fabfc1c9bc3a29
Zuger Cédric <zuger.cedric@gmail.com>
2016-12-19 18:55:51 +0100

    Logs are paginated.

--- app/controllers/game_logs_controller.rb
+++ app/controllers/game_logs_controller.rb
@@ -3,7 +3,7 @@ class GameLogsController < ApplicationController
   before_action :set_adventure
 
   def show
-    @log = @adventure.game_logs.includes( :page ).order( 'id DESC' )
+    @log = @adventure.game_logs.includes( :page ).order( 'id DESC' ).paginate(:page => params[:page])
   end
 
   private
