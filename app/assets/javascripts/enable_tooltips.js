281cb19c3d07bde59457cece954813d1f31b7297
ced <zuger.cedric@gmail.com>
2018-10-18 19:20:13 +0200

    Hero page modified and menu helpers fixed.

--- app/assets/javascripts/enable_tooltips.js
+++ app/assets/javascripts/enable_tooltips.js
@@ -1,3 +1,7 @@
 $( document ).on('turbolinks:load', function() {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip(
+        {
+            'delay': { show: 800 },
+            'placement': 'bottom'
+        })
 })
