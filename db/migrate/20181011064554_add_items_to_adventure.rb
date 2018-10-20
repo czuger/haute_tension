f71851bbf4ec72b3a470eccc21c711b2b6f23359
ced <zuger.cedric@gmail.com>
2018-10-20 14:14:20 +0200

    Fixing migration issue.

--- db/migrate/20181011064554_add_items_to_adventure.rb
+++ db/migrate/20181011064554_add_items_to_adventure.rb
@@ -1,7 +1,7 @@
 class AddItemsToAdventure < ActiveRecord::Migration[5.0]
   def change
-    add_column :adventures, :items, :string
-    drop_table :items
+    # add_column :adventures, :items, :string
+    # drop_table :items
     drop_table :adventures_items
   end
 end
