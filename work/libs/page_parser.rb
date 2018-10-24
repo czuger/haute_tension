0b86c3eaf4a51521af66e72973624d1c1b6379ec
ced <zuger.cedric@gmail.com>
2018-10-24 16:46:22 +0200

    Links process conversion improved.

--- work/libs/page_parser.rb
+++ work/libs/page_parser.rb
@@ -12,13 +12,13 @@ class PageParser
     # pp @pages_converter
 
     doc = Nokogiri::HTML( open( page_path ) )
-    downloaded_page = doc.css('div.ob-text')
+    read_page = doc.css('div.ob-text')
 
     text = []
     monsters = []
-    downloaded_page.children.each do |node|
+    read_page.children.each do |node|
       if node.class == Nokogiri::XML::Element
-        node = check_for_link( node )
+        node = process_links( node )
 
         # monsters = check_for_monsters( node, monsters )
         text << node.to_s.strip
@@ -37,42 +37,51 @@ class PageParser
 
   private
 
-  def check_for_monsters( node, monsters )
+  # def check_for_monsters( node, monsters )
+  #
+  #   if node.children.first.name == 'strong'
+  #     # p node
+  #     # p node.text
+  #     m = node.text.match( /(\d+) *VIE *: *(\d+)/ )
+  #     if m
+  #       # p node
+  #       monsters << { name: node.children.first.text, strength: m[1].to_i, hp: m[2].to_i }
+  #     else
+  #       puts "Monster in bad format : #{@origin_url}, #{node.inspect}"
+  #     end
+  #   end
+  #
+  #   monsters
+  # end
 
-    if node.children.first.name == 'strong'
-      # p node
-      # p node.text
-      m = node.text.match( /(\d+) *VIE *: *(\d+)/ )
-      if m
-        # p node
-        monsters << { name: node.children.first.text, strength: m[1].to_i, hp: m[2].to_i }
-      else
-        puts "Monster in bad format : #{@origin_url}, #{node.inspect}"
-      end
-    end
-
-    monsters
-  end
-
-  def check_for_link( node )
-    if node.children.first.name == 'a'
-      original_url = node.children.first.attributes['href'].value
-      # page = Page.find_by_url( original_url )
-      # raise "Unable to find page for #{original_url}" unless page
-      # This read the rails route
-      node.children.first.attributes['href'].value = "/adventures/read_choice/#{@pages_converter[original_url]}"
-      node.children.first['class'] = 'pageLink'
-      # p node.children.first
-    elsif ( anchor = node.css( 'a' ) )
-      if ! anchor.empty?
-        original_url = anchor.first.attributes['href'].value
-        # page = Page.find_by_url( original_url )
-        # raise "Unable to page page for #{original_url}" unless page
-        anchor.first.attributes['href'].value = "/adventures/read_choice/#{@pages_converter[original_url]}"
-        node.children.first['class'] = 'pageLink'
-      end
+  def process_links( node )
+    node.xpath( './/a' ).each do |link|
+      original_url = link.attributes['href'].value
+      link.attributes['href'].value = "/adventures/read_choice/#{@pages_converter[original_url]}"
+      link['class'] = 'pageLink'
     end
     node
   end
+
+  # def check_for_link( node )
+  #   if node.children.first.name == 'a'
+  #     original_url = node.children.first.attributes['href'].value
+  #     # page = Page.find_by_url( original_url )
+  #     # raise "Unable to find page for #{original_url}" unless page
+  #     # This read the rails route
+  #     node.children.first.attributes['href'].value = "/adventures/read_choice/#{@pages_converter[original_url]}"
+  #     node.children.first['class'] = 'pageLink'
+  #     # p node.children.first
+  #   elsif ( anchor = node.css( 'a' ) )
+  #     if ! anchor.empty?
+  #       original_url = anchor.first.attributes['href'].value
+  #       # page = Page.find_by_url( original_url )
+  #       # raise "Unable to page page for #{original_url}" unless page
+  #       anchor.first.attributes['href'].value = "/adventures/read_choice/#{@pages_converter[original_url]}"
+  #       node.children.first['class'] = 'pageLink'
+  #     end
+  #   end
+  #   node
+  # end
 
 end
