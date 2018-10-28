4b2d2d648eaaa76d71be16a46aae7991b62c3108
ced <zuger.cedric@gmail.com>
2018-10-28 12:19:34 +0100

    Bugs can be saved.

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -8,4 +8,21 @@ class BugsController < ApplicationController
     @bug = Bug.new( user: @user, page: @page )
   end
 
+  def create
+    @bug = Bug.new( bug_params )
+    @bug.user_id = @user.id
+
+    if @bug.save
+      redirect_to new_bug_path, notice: 'Merci pour votre contribution'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bug_params
+    params.require(:bug).permit(:info, :page_id)
+  end
+
 end
--- app/helpers/adventures_helper.rb
+++ app/helpers/adventures_helper.rb
@@ -2,8 +2,12 @@ module AdventuresHelper
 
   def print_summary( adventure )
     adventure_text = adventure.current_page&.text&.first
-    if adventure_text
-      return raw( adventure_text.gsub( '<p>', '' ).gsub( '</p>', '' ).truncate_words( 80 ) )
+    print_text_summary adventure_text
+  end
+
+  def print_text_summary( text, nb_words= 80 )
+    if text
+      return raw( text.gsub( '<p>', '' ).gsub( '</p>', '' ).truncate_words( nb_words ) )
     end
 
     ''
