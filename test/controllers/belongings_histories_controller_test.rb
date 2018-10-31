da8383b1fd1073017f3443619552c8374d9c405a
ced <zuger.cedric@gmail.com>
2018-10-31 19:19:01 +0100

    Fixing tests.

--- test/controllers/belongings_histories_controller_test.rb
+++ test/controllers/belongings_histories_controller_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class BelongingsHistoriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get belongings_histories_show_url
+
+  setup do
+    @user = create(:user)
+    sign_in @user
+
+    @book = create( :book )
+    @fight = create( :fight, book: @book, user: @user )
+    @adventure = create( :adventure, book: @book, current_page: @book.first_page, user: @user, current_fight: @fight, items: {} )
+    @user.update!( current_adventure_id: @adventure.id )
+    @belongings_history = create( :belongings_history, user: @user, book: @book )
+  end
+  
+  test 'should get show' do
+    get belongings_histories_url
     assert_response :success
   end
 
-  test "should get create" do
-    get belongings_histories_create_url
-    assert_response :success
+  test 'should get create' do
+    post belongings_histories_url, params: { history_id: @belongings_history.id }
+    assert_redirected_to belongings_url
   end
 
 end
