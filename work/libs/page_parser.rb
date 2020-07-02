f6508d0d444c4f830f95de916ef1b5bb2ff3b665
ced <zuger.cedric@gmail.com>
2020-07-02 15:11:22 +0200

    Page parsing reworked.

--- work/libs/page_parser.rb
+++ work/libs/page_parser.rb
@@ -1,7 +1,12 @@
 require 'nokogiri'
 require 'pp'
+require 'ostruct'
 
 class PageParser
+
+  def initialize
+    @pages = []
+  end
 
   def update( directory, parsing_hash, pages_converter )
 
@@ -9,22 +14,44 @@ class PageParser
     @origin_url = parsing_hash[:origin_url]
     @pages_converter = pages_converter
 
+    # puts
+    # puts "page_path = #{page_path}"
+    # puts "@origin_url = #{@origin_url}"
+
     doc = Nokogiri::HTML( open( page_path ) )
-    read_page = doc.css('div.ob-text')
 
-    text = []
-    monsters = []
-    read_page.children.each do |node|
-      if node.class == Nokogiri::XML::Element
-        node = process_links( node )
+    @pages << process_page( doc )
 
-        # monsters = check_for_monsters( node, monsters )
-        text << node.to_s.strip
-      end
-    end
+    # read_page = doc.css('div.ob-text')
+    #
+    # paragraphs = []
+    # monsters = []
+    # read_page.children.each do |node|
+    #   if node.class == Nokogiri::XML::Element
+    #     process_page( node )
+    #
+    #     # node = process_links( node )
+    #
+    #     # p node
+    #
+    #     # monsters = check_for_monsters( node, monsters )
+    #     paragraphs << node.to_s.strip
+    #
+    #     # p paragraphs
+    #
+    #   end
+    #
+    #   # p paragraphs
+    # end
 
-    File.open( "parsed_data/#{directory}/#{File.basename(page_path)}.yaml", 'w' ) do |f|
-      f.write( text.to_yaml )
+    # File.open( "parsed_data/#{directory}/#{File.basename(page_path)}.yaml", 'w' ) do |f|
+    #   f.write( paragraphs.to_yaml )
+    # end
+  end
+
+  def save( directory )
+    File.open( "parsed_data/#{directory}.yaml", 'w' ) do |f|
+      f.write( @pages.to_yaml )
     end
   end
 
@@ -47,13 +74,39 @@ class PageParser
   #   monsters
   # end
 
-  def process_links( node )
-    node.xpath( './/a' ).each do |link|
-      original_url = link.attributes['href'].value
-      link.attributes['href'].value = "/adventures/read_choice/#{@pages_converter[original_url]}"
-      link['class'] = 'pageLink'
+  def process_page( doc )
+    page = []
+
+    doc.css( '.ob-section-text' ).each do |text_section|
+
+      pictures = text_section.css( '.ob-img' )
+      page << OpenStruct.new( type: :pics, sources: pictures.map{ |p| p['src'] } ) unless pictures.empty?
+
+      text_section.css( '.ob-text' ).xpath( './/p' ).each do |p|
+        link = p.xpath( './/a' )
+
+        unless link.empty?
+          link = link[0]  # Shouldn't be more than one link
+          page << OpenStruct.new( type: :link, text: link.text, src: link['href'] )
+        else
+          page << OpenStruct.new( type: :text, text: p.text )
+        end
+      end
     end
-    node
+
+    page
   end
+
+  # def process_links( node )
+  #   node.xpath( './/a' ).each do |link|
+  #     original_url = link.attributes['href'].value
+  #
+  #     # p original_url
+  #
+  #     link.attributes['href'].value = "/adventures/read_choice/#{@pages_converter[original_url]}"
+  #     link['class'] = 'pageLink'
+  #   end
+  #   node
+  # end
 
 end
--- work/parse_raw_data.rb
+++ work/parse_raw_data.rb
@@ -3,7 +3,7 @@ require_relative 'libs/page_parser'
 require 'fileutils'
 require 'yaml'
 
-directories = %w( pretre_jean_forteresse_alamuth pretre_jean_mines_roi_salomon pretre_jean_mysteres_bablylone pretre_jean_oeil_sphinx )
+directories = %w( pretre_jean_forteresse_alamuth ) # pretre_jean_mines_roi_salomon pretre_jean_mysteres_bablylone pretre_jean_oeil_sphinx )
 
 directories.each do |directory|
   FileUtils.mkpath "parsed_data/#{directory}"
@@ -11,7 +11,14 @@ directories.each do |directory|
   db = YAML.load_file("raw_data/#{directory}.yaml")
   pages_converter = Hash[ db.values.map{ |e| [ e[:origin_url], e[:page_index] ] } ]
 
+  # p pages_converter
+
+  p_pars = PageParser.new
+
   db.values.each do |parsing_hash|
-    PageParser.new.update( directory, parsing_hash, pages_converter )
+    # p parsing_hash
+    p_pars.update( directory, parsing_hash, pages_converter )
   end
+
+  p_pars.save( directory )
 end
