8e5084c6fa79143f63df0142a0cfc3696f245783
ced <zuger.cedric@gmail.com>
2020-07-03 07:13:21 +0200

    can reset adventure and go back in case of failure.

--- vue_js/js/frontend.js
+++ vue_js/js/frontend.js
@@ -8,8 +8,22 @@ new Vue({
     },
     methods: {
         getUrl: function ( hash ) {
-            console.log( hash );
             this.current_hash = hash;
+            const h = LsManager.set_value( 'alamuth_current_hash', 'hash', hash );
+
+            let h_history = LsManager.get_value( 'alamuth_hash_history', 'history' ) || [];
+            h_history.push( hash );
+            LsManager.set_value( 'alamuth_hash_history', 'history', h_history );
+        },
+        reset: function () {
+            this.current_hash = '8ffd30fe683b4e639114068564522932464f4ba0170ad65f02c7897b85d55a3d';
+        },
+        back: function () {
+            let h_history = LsManager.get_value( 'alamuth_hash_history', 'history' ) || [];
+            h_history.pop();
+            const previous_hash = h_history.pop();
+            LsManager.set_value( 'alamuth_hash_history', 'history', h_history );
+            this.current_hash = previous_hash;
         }
     },
     watch: {
@@ -26,6 +40,8 @@ new Vue({
         }
     },
     mounted () {
-        this.current_hash = '8ffd30fe683b4e639114068564522932464f4ba0170ad65f02c7897b85d55a3d';
+        const h = LsManager.get_value( 'alamuth_current_hash', 'hash' );
+
+        this.current_hash = h || '8ffd30fe683b4e639114068564522932464f4ba0170ad65f02c7897b85d55a3d';
     }
 });
--- vue_js/js/local_storage.js
+++ vue_js/js/local_storage.js
@@ -4,6 +4,22 @@
 
 class LsManager{
 
+    static set_value( object_name, variable_name, value ){
+        var _data = localStorage.getItem( object_name );
+
+        if ( _data ) {
+            _data = JSON.parse(_data);
+        }
+        else {
+            _data = {};
+        }
+
+        _data[ variable_name ] = value;
+        _data = JSON.stringify(_data);
+
+        localStorage.setItem( object_name, _data );
+    }
+
     static get_value( object_name, variable_name ){
         var _data = localStorage.getItem( object_name );
 
